613d848de80ef29775762a87f533f0db42c03775
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-02 18:18:51 +0900

    added backend

--- web/index.js
+++ web/index.js
@@ -1,28 +1,38 @@
 // @ts-check
 import { join } from "path";
-import { readFileSync } from "fs";
+import fs from "fs";
 import express from "express";
 import cookieParser from "cookie-parser";
-import { Shopify, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { Shopify, ApiVersion, LATEST_API_VERSION } from "@shopify/shopify-api";
 
 import applyAuthMiddleware from "./middleware/auth.js";
 import verifyRequest from "./middleware/verify-request.js";
 import { setupGDPRWebHooks } from "./gdpr.js";
-import productCreator from "./helpers/product-creator.js";
-import redirectToAuth from "./helpers/redirect-to-auth.js";
 import { BillingInterval } from "./helpers/ensure-billing.js";
-import { AppInstallations } from "./app_installations.js";
+import { QRCodesDB } from "./qr-codes-db.js";
+import applyQrCodeApiEndpoints from "./middleware/qr-code-api.js";
 
 const USE_ONLINE_TOKENS = false;
+const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
 
 const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
+const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
+
+const versionFilePath = "./version.txt";
+let templateVersion = "unknown";
+if (fs.existsSync(versionFilePath)) {
+  templateVersion = fs.readFileSync(versionFilePath, "utf8").trim();
+}
 
 // TODO: There should be provided by env vars
 const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
 const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
-
+const dbFile = join(process.cwd(), "database.sqlite");
+const sessionDb = new Shopify.Session.SQLiteSessionStorage(dbFile);
+// Initialize SQLite DB
+QRCodesDB.db = sessionDb.db;
+QRCodesDB.init();
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
@@ -31,15 +41,31 @@ Shopify.Context.initialize({
   HOST_SCHEME: process.env.HOST.split("://")[0],
   API_VERSION: LATEST_API_VERSION,
   IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
+  SESSION_STORAGE: sessionDb,
 });
 
+// // const DB_PATH = `${process.cwd()}/database.sqlite`;
+
+// // Shopify.Context.initialize({
+// //   API_KEY: process.env.SHOPIFY_API_KEY,
+// //   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+// //   SCOPES: process.env.SCOPES.split(","),
+// //   HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
+// //   HOST_SCHEME: process.env.HOST.split("://")[0],
+// //   API_VERSION: LATEST_API_VERSION,
+// //   IS_EMBEDDED_APP: true,
+// //   // This should be replaced with your preferred storage strategy
+// //   SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
+// //   USER_AGENT_PREFIX: `Node App Template/${templateVersion}`,
+// // });
+
+// Storing the currently active shops in memory will force them to re-login when your server restarts. You should
+// persist this object in your app.
+const ACTIVE_SHOPIFY_SHOPS = {};
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
   path: "/api/webhooks",
-  webhookHandler: async (_topic, shop, _body) => {
-    await AppInstallations.delete(shop);
-  },
+  webhookHandler: async (topic, shop, body) =>
+    delete ACTIVE_SHOPIFY_SHOPS[shop],
 });
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
@@ -68,26 +94,24 @@ export async function createServer(
   billingSettings = BILLING_SETTINGS
 ) {
   const app = express();
-
+  app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
+  app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
   app.set("use-online-tokens", USE_ONLINE_TOKENS);
+
   app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
   applyAuthMiddleware(app, {
     billing: billingSettings,
   });
 
-  // Do not call app.use(express.json()) before processing webhooks with
-  // Shopify.Webhooks.Registry.process().
-  // See https://github.com/Shopify/shopify-api-node/blob/main/docs/usage/webhooks.md#note-regarding-use-of-body-parsers
-  // for more details.
   app.post("/api/webhooks", async (req, res) => {
     try {
       await Shopify.Webhooks.Registry.process(req, res);
       console.log(`Webhook processed, returned status code 200`);
-    } catch (e) {
-      console.log(`Failed to process webhook: ${e.message}`);
+    } catch (error) {
+      console.log(`Failed to process webhook: ${error}`);
       if (!res.headersSent) {
-        res.status(500).send(e.message);
+        res.status(500).send(error.message);
       }
     }
   });
@@ -101,11 +125,7 @@ export async function createServer(
   );
 
   app.get("/api/products/count", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
+    const session = await Shopify.Utils.loadCurrentSession(req, res, false);
     const { Product } = await import(
       `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
     );
@@ -114,37 +134,16 @@ export async function createServer(
     res.status(200).send(countData);
   });
 
-  app.get("/api/products/create", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
-    let status = 200;
-    let error = null;
-
-    try {
-      await productCreator(session);
-    } catch (e) {
-      console.log(`Failed to process products/create: ${e.message}`);
-      status = 500;
-      error = e.message;
-    }
-    res.status(status).send({ success: status === 200, error });
-  });
-
-  // All endpoints after this point will have access to a request.body
-  // attribute, as a result of the express.json() middleware
   app.use(express.json());
 
+  applyQrCodeApiEndpoints(app);
+
   app.use((req, res, next) => {
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
+    const shop = req.query.shop;
     if (Shopify.Context.IS_EMBEDDED_APP && shop) {
       res.setHeader(
         "Content-Security-Policy",
-        `frame-ancestors https://${encodeURIComponent(
-          shop
-        )} https://admin.shopify.com;`
+        `frame-ancestors https://${shop} https://admin.shopify.com;`
       );
     } else {
       res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
@@ -160,41 +159,33 @@ export async function createServer(
       ({ default: fn }) => fn
     );
     app.use(compression());
-    app.use(serveStatic(PROD_INDEX_PATH, { index: false }));
+    app.use(serveStatic(PROD_INDEX_PATH));
   }
 
   app.use("/*", async (req, res, next) => {
+    const shop = req.query.shop;
 
-    if (typeof req.query.shop !== "string") {
-      res.status(500);
-      return res.send("No shop provided");
+    // Detect whether we need to reinstall the app, any request from Shopify will
+    // include a shop in the query parameters.
+    if (app.get("active-shopify-shops")[shop] === undefined && shop) {
+      res.redirect(`/api/auth?shop=${shop}`);
+    } else {
+      // res.set('X-Shopify-App-Nothing-To-See-Here', '1');
+      const fs = await import("fs");
+      const fallbackFile = join(
+        isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
+        "index.html"
+      );
+      res
+        .status(200)
+        .set("Content-Type", "text/html")
+        .send(fs.readFileSync(fallbackFile));
     }
-
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
-    const appInstalled = await AppInstallations.includes(shop);
-
-    if (!appInstalled) {
-      return redirectToAuth(req, res, app);
-    }
-
-    if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== "1") {
-      const embeddedUrl = Shopify.Utils.getEmbeddedAppUrl(req);
-
-      return res.redirect(embeddedUrl + req.path);
-    }
-
-    const htmlFile = join(
-      isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
-      "index.html"
-    );
-
-    return res
-      .status(200)
-      .set("Content-Type", "text/html")
-      .send(readFileSync(htmlFile));
   });
 
   return { app };
 }
 
-createServer().then(({ app }) => app.listen(PORT));
+if (!isTest) {
+  createServer().then(({ app }) => app.listen(PORT));
+}
