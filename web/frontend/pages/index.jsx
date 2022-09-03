3cc194d871bd06b39266394e38a8a90c9ff61284
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-03 19:23:38 +0900

    fixing QR code

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -7,7 +7,6 @@ import {
   SkeletonBodyText,
 } from "@shopify/polaris";
 import { QRCodeIndex } from "../components";
-import { useAppQuery } from "../hooks";
 
 export default function HomePage() {
   /*
@@ -22,8 +21,6 @@ export default function HomePage() {
   */
   const isLoading = false;
   const isRefetching = false;
-
-  /* Mock data to display in the list */
   const QRCodes = [
     {
       createdAt: "2022-06-13",
@@ -55,8 +52,6 @@ export default function HomePage() {
       }
     },
   ];
-
-
 
   /* Set the QR codes to use in the list */
   const qrCodesMarkup = QRCodes?.length ? (
@@ -114,91 +109,3 @@ export default function HomePage() {
     </Page>
   );
 }
-
-
-// import {
-//   Card,
-//   Page,
-//   Layout,
-//   TextContainer,
-//   Image,
-//   Stack,
-//   Link,
-//   Heading,
-// } from "@shopify/polaris";
-// import { TitleBar } from "@shopify/app-bridge-react";
-
-// import { trophyImage } from "../assets";
-
-// import { ProductsCard } from "../components";
-
-// export default function HomePage() {
-//   return (
-//     <Page narrowWidth>
-//       <TitleBar title="App name" primaryAction={null} />
-//       <Layout>
-//         <Layout.Section>
-//           <Card sectioned>
-//             <Stack
-//               wrap={false}
-//               spacing="extraTight"
-//               distribution="trailing"
-//               alignment="center"
-//             >
-//               <Stack.Item fill>
-//                 <TextContainer spacing="loose">
-//                   <Heading>Nice work on building a Shopify app 🎉</Heading>
-//                   <p>
-//                     Your app is ready to explore! It contains everything you
-//                     need to get started including the{" "}
-//                     <Link url="https://polaris.shopify.com/" external>
-//                       Polaris design system
-//                     </Link>
-//                     ,{" "}
-//                     <Link url="https://shopify.dev/api/admin-graphql" external>
-//                       Shopify Admin API
-//                     </Link>
-//                     , and{" "}
-//                     <Link
-//                       url="https://shopify.dev/apps/tools/app-bridge"
-//                       external
-//                     >
-//                       App Bridge
-//                     </Link>{" "}
-//                     UI library and components.
-//                   </p>
-//                   <p>
-//                     Ready to go? Start populating your app with some sample
-//                     products to view and test in your store.{" "}
-//                   </p>
-//                   <p>
-//                     Learn more about building out your app in{" "}
-//                     <Link
-//                       url="https://shopify.dev/apps/getting-started/add-functionality"
-//                       external
-//                     >
-//                       this Shopify tutorial
-//                     </Link>{" "}
-//                     📚{" "}
-//                   </p>
-//                 </TextContainer>
-//               </Stack.Item>
-//               <Stack.Item>
-//                 <div style={{ padding: "0 20px" }}>
-//                   <Image
-//                     source={trophyImage}
-//                     alt="Nice work on building a Shopify app"
-//                     width={120}
-//                   />
-//                 </div>
-//               </Stack.Item>
-//             </Stack>
-//           </Card>
-//         </Layout.Section>
-//         <Layout.Section>
-//           <ProductsCard />
-//         </Layout.Section>
-//       </Layout>
-//     </Page>
-//   );
-// }
